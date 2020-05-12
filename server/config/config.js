fc4766ef4cc0314c9a52890fb7db26556d2810f1
charly <coleccionista84@gmail.com>
2020-05-12 16:22:39 -0600

    Variable de enterno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ylrahc:KOXWSGukYOmxktDF@cluster0-sdnzk.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.mongo_uri;
 }
 
 process.env.URLDB = urlDB;
