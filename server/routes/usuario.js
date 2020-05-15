3e2bf145f03e74238512c491d405949b3543573b
charly <coleccionista84@gmail.com>
2020-05-15 14:20:04 -0600

    Actualizacion Crear Usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -40,7 +40,7 @@ app.get('/usuario', verificaToken, (req, res) => {
         })
 });
 
-app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
+app.post('/usuario', function(req, res) {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
